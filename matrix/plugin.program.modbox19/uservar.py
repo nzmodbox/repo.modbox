e6d08f9d25ecb9e832d2f2b08bb3ebb68899b761
nzmodbox <pbrobyn@hotmail.com>
2021-09-06 19:15:44 +1200

    updated

--- matrix/plugin.program.modbox19/uservar.py
+++ matrix/plugin.program.modbox19/uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = '[COLOR limegreen][B]Modbox[/B][/COLOR]Wizard'
 BUILDERNAME = 'ModboxWizard'
 EXCLUDES = [ADDON_ID, 'repository.modbox']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
-BUILDFILE = 'http://'
+BUILDFILE = 'https://github.com/nzmodbox/nzmodbox.com/blob/master/builds.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
