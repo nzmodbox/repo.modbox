83b2bb12e4a36cb1edcc63226fd8dfba29a1f007
nzmodbox <pbrobyn@hotmail.com>
2023-09-25 00:27:01 +1300

    update

---
+++ Nexus/plugin.program.modboxwiz/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'http://CHANGEME'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'http://slamiousproject.com/wzrd/notify19.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
